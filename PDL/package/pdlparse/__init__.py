d1455c67c9eab170517a45f3d7347570d0571a1e
Cooper <Cooperransom08@gmail.com>
2023-04-30 23:48:37 -0400

    create new file

--- PDL/package/pdlparse/__init__.py
+++ PDL/package/pdlparse/__init__.py
@@ -1,1 +1,3 @@
+#https://www.freecodecamp.org/news/build-your-first-python-package/
 from parse import scrap_library, get_variable, filter_file, get_strings, get_class, get_type, get_value, get_comments
+from library import get_libs, get_lib_info, get_main_lib, read_lib, format_lib
---
+++ PDL/package/pdlparse/library.py
@@ -0,0 +1,97 @@
+import os, sys
+
+
+def get_libs(base_dir):
+  retlist = []
+  for r, d, f in os.walk(base_dir):
+    for file in f:
+      if '.pdl' in file:
+        itemobj = os.path.relpath(f'{r}/{file}')
+        retlist.append(itemobj)
+  
+  return retlist
+
+
+def get_lib_info(library):
+  if not os.path.exists(library):
+    raise FileNotFoundError(f'ERROR: Package pdlparse cannot find file {library}.')
+  
+  with open(library, 'r') as file:
+    file_content = file.read()
+  if '=' or '{' in file_content:
+    var_count = 0
+    class_count = 0
+    for line in file_content.splitlines():
+      if '=' in line:
+        var_count += 1
+      if '{' in line:
+        class_count += 1
+      else:
+        pass
+  
+  else:
+    raise Exception(f'ERROR: Package pdlparse cannot find vars or classes in {library}.')
+  
+  return f'variable# = {var_count} class# = {class_count}'
+
+
+def get_main_lib(base_dir):
+  if not os.path.exists(base_dir):
+    raise FileNotFoundError(f'ERROR: Package pdlparse cannot find file {base_dir}.')
+  
+  retlist = []
+  for r, d, f in os.walk(base_dir):
+    for file in f:
+      if 'main' in file:
+        if '.pdl' in file:
+          itemobj = os.path.relpath(f'{r}/{file}')
+          retlist.append(itemobj)
+        else:
+          pass
+  
+  if len(retlist) == 0:
+    retlist.append('No main library found.')
+  return retlist
+
+
+def read_lib(library, aloud=False):
+  if not os.path.exists(library):
+    raise FileNotFoundError(f'ERROR: Package pdlparse cannot find file {library}.')
+  
+  with open(library, 'r') as file:
+    retlist = []
+    file_content = file.read()
+    
+  for line in file_content.splitlines():
+    if aloud != False:
+      retlist.append(line)
+    else:
+      print(line)
+  if aloud != False:
+    return retlist
+
+
+def format_lib(library):
+  if not os.path.exists(library):
+    raise FileNotFoundError(f'ERROR: Package pdlparse cannot find file {library}.')
+  
+  with open(library, 'r') as file:
+    file_content = file.read()
+  
+  for line in file_content.splitlines():
+    if '}' in line:
+      index = line.find('}')
+      if not ';' in line:
+        with open(library, 'w') as edit:
+          replacement = file_content.replace('}', '};')
+          edit.write(replacement)
+    if '{' in line:
+      index = line.find('{')
+      name = line[0:index-1]
+      if not 'class' in line:
+        with open(library, 'w') as edit:
+          replacement = file_content.replace(f'{name}', f'class {name}')
+          edit.write(replacement)
+    else:
+      pass
+    
